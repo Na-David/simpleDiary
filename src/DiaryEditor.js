59882935f710fd03c1034ef97711a2acdc6a564f
David Na <hna206@naver.com>
2023-01-30 17:46:39 +1300

    finalized with css

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -4,6 +4,7 @@ const DiaryEditor = () => {
     const [state,setState] = useState({
         author: "",
         content: "",
+        grade: "Best"
     });
 
     const handleChangeState = (e) => {
@@ -11,6 +12,11 @@ const DiaryEditor = () => {
             ...state,
             [e.target.name]: e.target.value
         });
+    };
+
+    const handleSubmit = () => {
+        console.log(state);
+        alert("Saved");
     };
 
     return (
@@ -31,9 +37,23 @@ const DiaryEditor = () => {
             onChange= {handleChangeState}
             />
         </div>
+
+        <div>
+        <label>Today's rating is: </label>
+            <select name = "grade" value ={state.grade} onChange = {handleChangeState}>
+                <option value = {1}>Best</option>
+                <option value = {2}>Good</option>
+                <option value = {3}>Soso</option>
+                <option value = {4}>Not Satisfied</option>
+                <option value = {5}>...</option>
+            </select>
+        </div>
+
+        <div>
+
+            <button onClick={handleSubmit}>Save</button>
+        </div>
     </div>)
-
-
 }
 
 export default DiaryEditor;
