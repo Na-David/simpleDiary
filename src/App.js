a0b6ab038f145b5d5f4726e2291c3abb2db9a0d8
David Na <hna206@naver.com>
2023-02-25 01:29:27 +1300

    useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import COunter from './counter';
 import MyHeader from './MyHeader';
 
 function App() {
   return (
     <div className="App">
         <MyHeader />
+        <COunter />
         <h2>Hello World</h2>
         <b id='bold_text'>React.js</b>
     </div>
