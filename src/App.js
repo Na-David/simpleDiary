22bc2f42c5e7a48d9f72c344920a671e2b877f8d
David Na <hna206@naver.com>
2023-02-26 00:48:54 +1300

    DiaryList(dummy) rendering

--- src/App.js
+++ src/App.js
@@ -2,11 +2,42 @@ import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
+const dummyList = [
+  {
+    id: 1,
+    author: "David",
+    content: "Genesis",
+    emotion: 1,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "Paul",
+    content: "Exodus",
+    emotion: 2,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "Matthew",
+    content: "Luke",
+    emotion: 3,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 4,
+    author: "John",
+    content: "Deutronomy",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+]
+
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
-      <DiaryList />
+      <DiaryList diaryList = {dummyList} />
     </div>
   );
 }
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,9 +1,24 @@
-const DiaryList = () => {
+const DiaryList = ({diaryList}) => {
+
     return (
         <div className="DIaryList">
             <h2>일기리스트</h2>
+            <h4>총 {diaryList.length}개의 일기가 있습니다.</h4>
+            <div>
+                {diaryList.map((it, idx) => (
+                <div key={idx}>
+                    <div>작성자 : {it.author}</div>
+                    <div>일기 : {it.content}</div>
+                    <div>감정 : {it.emotion}</div>
+                    <div>시간 : {it.created_date}</div>
+                </div>))}
+            </div>
         </div>
     )
 }
+
+DiaryList.defaultProps = {
+    diaryList: []
+};
 
 export default DiaryList;
